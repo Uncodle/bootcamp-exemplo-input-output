33f16c17130420a48089826a816940a9fbcb61ce
Uncodle <ribeiro.geovana@outlook.com>
2022-06-20 13:04:02 -0300

    create example of @input and @output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public hasAnswer = false;
+  public userResponse;
+
+  public getAnswer(userResponse: boolean) {
+    this.userResponse = userResponse === true ? 'Gostaria' : 'Não gostaria';
+    this.hasAnswer = true;
+  }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>Olá, {{name}}!</h1>
+            Você já ouviu falar dos produtos Ivone?
+            <br>
+            <br>
+            <button (click)="sendAnswer(true)">Sim</button>
+            <button (click)="sendAnswer(false)">Não</button>
+  `,
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+  @Output() answerFromUser: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  public sendAnswer(value: boolean) {
+    this.answerFromUser.emit(value);
+  }
 }
